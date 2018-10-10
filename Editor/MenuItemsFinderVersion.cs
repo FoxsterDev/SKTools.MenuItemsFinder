da85e5486d00534b7cdd9e45f241de72c06abf58
Sergey Khalandachev <s.khalandachev@gmail.com>
2018-10-11 01:13:28 +0300

    [MenuItemsFinder] update screen + level up version

--- Editor/MenuItemsFinderVersion.cs
+++ Editor/MenuItemsFinderVersion.cs
@@ -4,7 +4,11 @@ namespace SKTools.MenuItemsFinder
 {
     internal class MenuItemsFinderVersion
     {
+        //0.1.4 added new feature managing missed items
+
+        //0.1.3 added new feature to open file that contains some item
+
         //0.1.2 added new feature to set custom id for menuitems and searching by custom id
-        public static readonly Version Version = new Version(0, 1, 2);
+        public static readonly Version Version = new Version(0, 1, 4);
     }
 }
