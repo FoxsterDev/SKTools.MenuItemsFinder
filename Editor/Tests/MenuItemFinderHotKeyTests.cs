1a6540aefbafb87bdb57ef367b68486509fedd8c
Sergey Khalandachev <s.khalandachev@gmail.com>
2018-11-01 01:31:38 +0300

    [MenuItemsFinder] in process "Add support unity package manager to open a script that contains some menuitem"

---
+++ Editor/Tests/MenuItemFinderHotKeyTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace SKTools.MenuItemsFinder.Tests
+{
+    public class HotKeys
+    {
+        [Test]
+        public void ExtractHotKey()
+        {
+            var index = -1;
+            var hotkey = "";
+            var key = "";
+            var shift = false;
+            var alt = false;
+            var cmd = false;
+
+            MenuItemHotKey.ExtractFrom("Window/Analysis/Profiler %&7", out index, out hotkey, out key, out shift,
+                out alt, out cmd);
+            Assert.IsTrue(hotkey == "%&7");
+            Assert.IsTrue(cmd & alt);
+            Assert.IsTrue(key == "7");
+        }
+
+        [Test]
+        public void ExtractHotKeyWithUnderScore()
+        {
+            var index = -1;
+            var hotkey = "";
+            var key = "";
+            var shift = false;
+            var alt = false;
+            var cmd = false;
+
+            MenuItemHotKey.ExtractFrom("Window/Analysis/Profiler _g", out index, out hotkey, out key, out shift,
+                out alt, out cmd);
+            Assert.IsTrue(hotkey == "g");
+            Assert.IsTrue(key == "g");
+        }
+
+        [Test]
+        public void CheckNotValidHotKey()
+        {
+            var index = -1;
+            var hotkey = "";
+            var key = "";
+            var shift = false;
+            var alt = false;
+            var cmd = false;
+
+            MenuItemHotKey.ExtractFrom("Window/Analysis/Profiler_g", out index, out hotkey, out key, out shift, out alt,
+                out cmd);
+            Assert.IsTrue(hotkey == string.Empty);
+            Assert.IsFalse(cmd);
+            Assert.IsFalse(shift);
+            Assert.IsFalse(alt);
+        }
+
+        [Test]
+        public void CheckEmptyHotKey()
+        {
+            var index = -1;
+            var hotkey = "";
+            var key = "";
+            var shift = false;
+            var alt = false;
+            var cmd = false;
+
+            MenuItemHotKey.ExtractFrom("Window/Analysis/Profiler Some Some", out index, out hotkey, out key, out shift,
+                out alt, out cmd);
+            Assert.IsTrue(hotkey == string.Empty);
+            Assert.IsFalse(cmd);
+            Assert.IsFalse(shift);
+            Assert.IsFalse(alt);
+        }
+
+        [Test]
+        public void ExtractHotkeyWithSpecialKeyboardSymbols()
+        {
+            var index = -1;
+            var hotkey = "";
+            var key = "";
+            var shift = false;
+            var alt = false;
+            var cmd = false;
+
+            MenuItemHotKey.ExtractFrom("Window/Analysis/Profiler #LEFT", out index, out hotkey, out key, out shift,
+                out alt, out cmd);
+            Assert.IsTrue(hotkey == "#LEFT");
+            Assert.IsTrue(shift);
+            Assert.IsTrue(key == "LEFT");
+        }
+    }
+}
---
+++ Editor/Tests/MenuItemFinderPackageManagerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace SKTools.MenuItemsFinder.Tests
+{
+    public class  PackageManager
+    {
+        [Test]
+        public void Start()
+        {
+           MenuItemsFinder.PrintProjectPackages();
+        }
+    }
+
+}
