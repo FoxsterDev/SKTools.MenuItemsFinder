2f2456cbd20f43bd2c46ef939605b8fae0508957
Sergey Khalandachev <s.khalandachev@gmail.com>
2019-02-02 14:41:43 +0300

    [MenuItemsFinder] add unity hotkeys link

--- Editor/MenuItems Collection/MenuItemsCollection.Links.cs
+++ Editor/MenuItems Collection/MenuItemsCollection.Links.cs
@@ -5,16 +5,23 @@ namespace SKTools.MenuItemsFinder
 {
     internal partial class MenuItemsCollection
     {
-        [MenuItem(MenuAssetPath + "Links/Platform dependent compilation", false, Priority)]
+        [MenuItem(MenuAssetPath + "Links/Unity/Platform dependent compilation", false, Priority)]
         public static void OpenPlatformDependentCompilation()
         {
             Application.OpenURL(@"https://docs.unity3d.com/Manual/PlatformDependentCompilation.html");
         }
 
-        [MenuItem(MenuAssetPath + "Links/Log Files", false, Priority)]
+        [MenuItem(MenuAssetPath + "Links/Unity/Log Files", false, Priority)]
         public static void OpenLogFiles()
         {
             Application.OpenURL(@"https://docs.unity3d.com/Manual/LogFiles.html");
+        }
+
+        [MenuItem(MenuAssetPath + "Links/Unity/Hotkeys", false, Priority)]
+
+        public static void OpenUnityHotkeys()
+        {
+            Application.OpenURL(@"https://docs.unity3d.com/Manual/UnityHotkeys.html");
         }
     }
 }
